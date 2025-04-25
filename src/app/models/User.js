e2939754a8a87408738a1095566dda2b6e645a1c
ViniRivaldar <vinicius-rivaldar@outlook.com>
2025-04-25 21:07:53 +0000

    criando tabela User

---
+++ src/app/models/User.js
@@ -0,0 +1,36 @@
+import Sequelize, {Model} from 'sequelize';
+
+class User extends Model{
+  static init(sequelize){
+    super.init({
+      id:{
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username:{
+        type: Sequelize.STRING,
+        allowNull: false,
+         unique: true,
+      },
+      email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    }, {
+      sequelize
+    })
+  }
+}
+
+export default User
---
+++ src/database/migrations/20250425210156-create-users.cjs
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
