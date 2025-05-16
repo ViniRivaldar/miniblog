13a6f04ae0ee21e7899ec7b86d5807e26bfc4b06
Vinicius Rivaldar dos Santos Silva <149613214+ViniRivaldar@users.noreply.github.com>
2025-05-16 10:54:57 -0300

    Update FotoPost.js

--- src/app/controllers/FotoPost.js
+++ src/app/controllers/FotoPost.js
@@ -21,11 +21,6 @@ class FotoPostController {
         return res.status(400).json({ error: 'O ID do post é obrigatório' });
       }
 
-      console.log("🧾 Dados recebidos para salvar no banco:");
-      console.log("originalmente:", originalname);
-      console.log("filename:", filename);
-      console.log("post_id:", post_id);
-
       const foto = await FotoPost.create({
         originalmente: originalname,
         filename: filename,
